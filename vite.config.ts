575ad14829f6c035bd9a1a88fa87f7cabe9f96db
ridhamoswal2 <ridhamoswal2@gmail.com>
2025-06-18 01:21:01 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/',
   server: {
     host: "::",
     port: 8080,
@@ -18,5 +19,5 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
+  }
 }));
